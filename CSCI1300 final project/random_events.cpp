58a6b9c0d06797bd1904deb4f1a2e07554017521
meerab9 <mebi8312@colorado.edu>
2025-11-19 16:30:33 -0700

    Update random_events.cpp

--- CSCI1300 final project/random_events.cpp
+++ CSCI1300 final project/random_events.cpp
@@ -1,1 +1,15 @@
+#include "random_events.h"
+#include "UsefulFunctions.h"
 
+RandomEvents:: RandomEvents(){
+  question = "";
+  answer = "";
+  index = 0;
+}
+
+void RandomEvents:: setRandomIndex(){
+  int i = rand()// figure out how to set that random index
+  index = i;
+}
+
+void RandomEvents::
