23e6868e16f6a2e03ff48646667c4cace7bf9640
Nicholas Gowin <nicholasgowin1@gmail.com>
2025-11-21 14:05:19 -0700

    Update Character class methods and add new functionality

--- CSCI1300 final project/Characters.h
+++ CSCI1300 final project/Characters.h
@@ -2,27 +2,29 @@
 #include <fstream>
 #include <vector>
 #include "UsefulFunctions.h"
+
+
 using namespace std;
+
+#pragma once
+
+#ifndef CHARACTERS_H
+#define CHARACTERS_H
 
 class Character { 
    public:
    Character();
-   void setCharacterInfo(string fileName, int index);
+   void setCharacterInfo(string fileName, int index, int playerNum);
    //takes items from a file and sets the character's info accordingly
    void addDiscoveryPoints(bool questionCorrect);
    //determines whether to add discovery points based on if the question was answered correctly
 
-   void addAccuracy();
-   //if the player gets a streak of correct answers, their accuracy increases
+  
    void setAnswerStreak(bool questionCorrect);
    //determines how many questions are correct
-   void addEfficiency(int eff);
-    //if the player rolls above a certain number, their efficiency increases
-    //if the player rolls below a certain number, their efficiency decreases
-   void addInsight(int ins);
-   //if the player lands on certain tiles, their insight increases
-    //if the player lands on certain tiles, their insight decreases
-    void displayCharacters(string fileName);
+   
+    void displayAllCharacters(string fileName,int index);
+    void tallyUpPoints(bool ansCorrect, int roll, int luck);
     
 
    string getName();
@@ -31,11 +33,10 @@ class Character {
    int getEfficiency();
    int getInsight();
    int getDiscoveryPoints();
+   int getFinalPoints();
 
+   void selectionSort(Character list[],int size);
    
-   
-   
-
    private:
     string name;
     int experience;
@@ -48,4 +49,16 @@ class Character {
     int incorrectStreak;
     int indexOfChosenCharacter;
 
+    void addAccuracy();
+    //if the player gets a streak of correct answers, their accuracy increases
+    void addEfficiency(int eff);
+    //if the player rolls above a certain number, their efficiency increases
+    //if the player rolls below a certain number, their efficiency decreases
+    void addInsight(int ins);
+    //if the player lands on certain tiles, their insight increases
+    //if the player lands on certain tiles, their insight decreases
+
+    bool isSorted(double list[],int size);
 };
+
+#endif
