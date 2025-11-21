4b84484036b3a8d5a962ee566467fa0b8d7d4957
meerab9 <mebi8312@colorado.edu>
2025-11-21 09:39:34 -0700

    Update random_events.h

--- CSCI1300 final project/random_events.h
+++ CSCI1300 final project/random_events.h
@@ -6,16 +6,19 @@ using namespace std;
 class RandomEvents {
   public: 
     RandomEvents();
-    string getAnswer();
-    void setAnswer(string fileName);
-    string getQuestion ();
-    void setQuestion(string fileName);
+    string getPathType();
+    void setPathType(string fileName);
+    string getAdvisor();
+    void setAdvisor(string fileName);
+    string getPoints();
+    void setPoints(string fileName);
     void setRandomIndex();
-    bool checkAnswer(string userAnswer);
+    
 
   private:
-    string answer;
-    string question;
+    string path;
+    string advisor;
+    string points;
     int index;
 
 };
