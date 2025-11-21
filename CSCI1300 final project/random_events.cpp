404ae195fb4545833df1999cedc6a8f49acbe937
meerab9 <mebi8312@colorado.edu>
2025-11-21 14:02:52 -0700

    Update random_events.cpp

--- CSCI1300 final project/random_events.cpp
+++ CSCI1300 final project/random_events.cpp
@@ -2,8 +2,9 @@
 #include "UsefulFunctions.h"
 
 RandomEvents:: RandomEvents(){
-  question = "";
-  answer = "";
+  path = "";
+  advisor = "";
+  points = "";
   index = 0;
 }
 
@@ -12,10 +13,66 @@ void RandomEvents:: setRandomIndex(){
   index = i;
 }
 
-void RandomEvents::
+void RandomEvents::setPathType(string fileName){
+  ifstream inputFile(fileName);
+  string line;
+  int currentIndex = 0;
+  if(inputFile.is_open()){
+      while(getline(inputFile,line)){
+        if(currentIndex == index){
+          vector<string> words;
+          split(line,words,'|');
+          path = words[1];
+          break;
+        }
+        currentIndex++;
+      }
+  }
+inputFile.close();
+}
+
+void RandomEvents::setAdvisor(string fileName){
+  ifstream inputFile(fileName);
+  string line;
+  int currentIndex = 0;
+  if(inputFile.is_open()){
+    while(getline(inputFile,line)){
+      if(currentIndex == index){
+        vector<string> words;
+        split(line,words,'|');
+        path = words[1];
+        advisor = words[2];
+        break;
+      }
+      currentIndex++;
+    }
+  }
+inputFile.close();
+}
+
+void RandomEvents::setPoints(string fileName){
+  ifstream inputFile(fileName);
+  string line;
+  int currentIndex = 0;
+  if(inputFile.is_open()){
+    while(getline(inputFile,line)){
+      if(currentIndex == index){
+        vector<string> words;
+        split(line,words,'|');
+        path = words[1];
+        advisor = words[2];
+        points = words[3];
+        break;
+      }
+      currentIndex++;
+    }
+  }
+inputFile.close();
+}
 
 //have a void to set the random event
 //have a void to get the user path, and then the path for the event
 //have a void to get the user advisor and then the advisor for the event
 //have a void to get the user points and then the points added or subtracted for the event. 
 //pink and purple are the random events squares
+//have a class to add and subtract the points -- use from the characters 
