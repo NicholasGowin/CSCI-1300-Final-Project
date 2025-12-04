7aaacea05671bfb60a502b2e808be665fa7fdc43
meerab9 <mebi8312@colorado.edu>
2025-12-04 14:05:18 -0700

    DNA Class

---
+++ DNA.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+class DNA{
+    public:
+        DNA();
+        double strandSimilarity(); // userStrand1 and userStrand 2
+        int bestStrandMatch(); // matchStrand1 and matchStrand2
+        void identifyMutations(); // matchStrand1 and matchStrand2
+        void transcribeDNAtoRNA(); // strand
+        void userStrandSimilarity();
+        void userStrandMatch();
+        
+    private:
+        string userStrand1;
+        string userStrand2;
+        string matchStrand1;
+        string matchStrand2; 
+        string strand;
+        double similarityScore;
+        int bestMatchIndex;
+};
