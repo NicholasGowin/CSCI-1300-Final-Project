f9081ce24d36d2fb9a4f854b4468edc1ef2d2f89
meerab9 <mebi8312@colorado.edu>
2025-12-04 14:38:10 -0700

    Implement main function for DNA event selection

---
+++ DNAmain.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "DNA.h"
+using namespace std;
+
+int main(){
+    DNA dna;
+    int choice;
+    cout << "Choose a DNA Event: Enter the number corresponding to your choice: 1 - Similar Strands of Equal Length | 2 - Similar Strands of Unequal Length | 3 - Mutation Identifier | 4 - Transcribe DNA to RNA" << endl;
+    cin >> choice;
+    if(choice == 1){
+        dna.strandSimilarity();
+    }
+    else if (choice == 2){
+        dna.bestStrandMatch();
+    }
+    else if (choice == 3){
+        dna.identifyMutations();
+    }
+    else if (choice == 4){
+        dna.transcribeDNAtoRNA();
+    } 
+    else{
+        cout << "Invalid choice, please try again!" << endl;
+    }
+    return 0;
+};
